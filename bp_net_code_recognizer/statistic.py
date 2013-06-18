14f8f9c53f9ee55ac4e51eac23ed90f964e6f099
sniky <sniky.hack@gmail.com>
2013-06-18 21:56:16 +0800

    for article use

---
+++ bp_net_code_recognizer/statistic.py
@@ -0,0 +1,47 @@
+import os, os.path, sys, re
+from collections import Counter
+
+codes = ['c', 'py', 'java']
+chars = [')', '(', '_', '.', '=', ';', '"', ',', "'", '*', '/', '{', '}', ':', '-', '0', '+', '1', '[', ']']
+pat   = {'c': "1 0 0", 'py': "0 1 0", 'java': "0 0 1"}
+
+#rats = cnts = {'c':Counter(chars), 'py': Counter(chars), 'java': Counter(chars)}
+#tots = {'c': 0, 'py': 0, 'java': 0}
+
+def showFile(args, path, fname):
+	for f in fname:
+		rats = cnts = {'c':Counter(chars), 'py': Counter(chars), 'java': Counter(chars)}
+		tots = {'c': 0, 'py': 0, 'java': 0}
+		#print os.path.join(path, f)
+		tmpf = open(os.path.join(path, f), 'r')
+		tmps = tmpf.read()
+		tmpf.close()
+		tmps = re.sub(r'\s', '', tmps)
+
+		tots[args] += len(tmps)
+		if tots[args] < 200:
+			continue
+		#print tmps
+		for s in tmps:
+			if s in chars:
+				cnts[args][s] += 1
+		calRatios(args, rats, cnts, tots)
+def calRatios(code, rats, cnts, tots):
+	#print "Counter of #FILE<*.%s>:" % code
+	#print cnts[code]
+	tmps = ""
+	for i in range(len(chars)):
+		#print cnts[code][chars[i]]
+		rats[code][chars[i]] = '%.2f' % ((cnts[code][chars[i]] - 1.0) / tots[code])
+		tmps += str(rats[code][chars[i]]) + " "
+	tmps += "> %s\n" % pat[code]
+	#print "Ratios of #FILE<*.%s>:" % code
+	#print rats[code]
+	#print '\n'
+	trainSet = open('trainSet.txt', 'a')
+	trainSet.write(tmps)
+	trainSet.close()
+def main():
+	for c in codes:
+		os.path.walk(c, showFile, (c))
+main()
